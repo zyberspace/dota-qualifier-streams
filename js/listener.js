82d07f5b37ae7cc99ea063cb5a691c2ac6de4db3
zyberspace <zyberspace@zyberware.org>
2017-01-15 00:08:08 +0100

    Added manual reconnect in case the connection gets closed

--- js/listener.js
+++ js/listener.js
@@ -10,42 +10,53 @@ view.update({
     "streams": streams
 });
 
-//Initiate EventSource object
-const listener = new EventSource("https://jewel.zyberware.org:3220/");
-listener.onopen = event => {
-    view.update({
-        "connecting": false
-    });
+(function createListener() {
+    //Initiate EventSource object
+    let listener = new EventSource("https://jewel.zyberware.org:3220/");
 
-    //Wait till the connection bar is hidden before changing the text
-    setTimeout(() => {
+    listener.onopen = event => {
+        console.log("Connected to EventSource stream. readyState === ", listener.readyState);
         view.update({
-            "connectingType": "Reconnecting" //Next time we need to connect it's a "reconnect"
+            "connecting": false
         });
-    }, 3000);
-};
-listener.onerror = event => {
-    view.update({
-        "connecting": true
-    });
-};
 
-//Add listeners for the events
-listener.addEventListener("streams-update", event => {
-    let updatedStreams = JSON.parse(event.data);
-    for (const streamsId in updatedStreams) {
-        if (!streamsIdToIndex[streamsId]) {
-            streamsIdToIndex[streamsId] = streams.length;
-            streams.push(null);
+        //Wait till the connection bar is hidden before changing the text
+        setTimeout(() => {
+            view.update({
+                "connectingType": "Reconnecting" //Next time we need to connect it's a "reconnect"
+            });
+        }, 3000);
+    };
+    listener.onerror = event => {
+        console.log("Error with EventSource stream. readyState === ", listener.readyState);
+        view.update({
+            "connecting": true
+        });
+
+        if (listener.readyState === 2) { //CLOSED
+            //Manually retry after 3 seconds because firefox has problems with this
+            listener.close();
+            setTimeout(() => createListener(), 3000);
         }
+    };
 
-        //Update array values with splice so rivets recognizes them
-        streams.splice(streamsIdToIndex[streamsId], 1, updatedStreams[streamsId]);
-    }
-});
+    //Add listeners for the events
+    listener.addEventListener("streams-update", event => {
+        let updatedStreams = JSON.parse(event.data);
+        for (const streamsId in updatedStreams) {
+            if (!streamsIdToIndex[streamsId]) {
+                streamsIdToIndex[streamsId] = streams.length;
+                streams.push(null);
+            }
 
-listener.addEventListener("page-viewers-update", event => {
-    view.update({
-        "page-viewers": event.data
+            //Update array values with splice so rivets recognizes them
+            streams.splice(streamsIdToIndex[streamsId], 1, updatedStreams[streamsId]);
+        }
     });
-});
+
+    listener.addEventListener("page-viewers-update", event => {
+        view.update({
+            "page-viewers": event.data
+        });
+    });
+})();
